332f12efcabe6e49a260a08a26b5bddbf28b030a
Doug Crawford <doug.crawford@intelight-its.com>
2019-05-17 16:34:23 +0000

    v3.02

--- fio/src/fiodriver/fioman.c
+++ fio/src/fiodriver/fioman.c
@@ -5224,7 +5224,7 @@ fioman_ioctl
 		case FIOMAN_IOC_VERSION_GET:
 		{
 			FIO_IOC_VERSION_GET *p_arg = (FIO_IOC_VERSION_GET *)arg;
-			char ver[80] = "Intelight, 3.01, 2.17";
+			char ver[80] = "Intelight, 3.02, 2.17";
 			
 			if (copy_to_user(p_arg, ver, strlen(ver) )) {
 				/* Could not copy for some reason */
